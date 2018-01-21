5c6ad87468489b66f1e7da110c8069626ae32082
Tyson Hood <TysonHood@gmail.com>
2018-01-20 18:55:26 -0800

    Final touches.

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ function updateClock() {
 
 	// get today's day
 	var js_date = new Date();
-	var newDay = js_date.getDay();
 
 	// Set today's day into html
 	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	var today = days[js_date.getDay()];
-
 
 	document.getElementById('day').innerHTML = today;
 
@@ -22,7 +20,11 @@ function updateClock() {
 	// Fix hours so they display in non-military time
 	if (newHours > 12) {
 		newHours = newHours - 12;
+		
+		// if it's after noon, change am to pm
+		document.getElementById('ampm').innerHTML = "pm";
 	}
+
 	// Fix minutes & seconds to display a zero if less than 10
 	if (newMinutes < 10) {
 		newMinutes = "0" + newMinutes;
@@ -31,6 +33,8 @@ function updateClock() {
 	if (newSeconds < 10) {
 		newSeconds = "0" + newSeconds;
 	}
+
+
 
 	// set today's time
 	document.getElementById('hours').innerHTML = newHours;
