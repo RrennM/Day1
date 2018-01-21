59ff9031da23c9972f38dc7b9d2d914dcb0361d4
Tyson Hood <TysonHood@gmail.com>
2018-01-20 17:11:43 -0800

    Make today appear.

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 var day = document.getElementById('day');
 console.log(day);
 
-var time = document.getElementById('time');
-console.log(time);
-
 var js_date = new Date();
 var newDay = js_date.getDay();
 console.log(newDay);
 
-var newTime = js_date.getTime();
 var newHours = js_date.getHours();
 var newMinutes = js_date.getMinutes();
 var newSeconds = js_date.getSeconds();
-console.log(newTime)
 console.log(newHours)
 console.log(newMinutes)
 console.log(newSeconds)
 
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = days[js_date.getDay()];
+console.log(today)
+
+day.innerHTML = today;
